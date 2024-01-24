81534de23533929ff855b6f4623b7a0892437b52
Raphaël Velt <raphv@users.noreply.github.com>
2024-01-24 20:56:57 +0100

    Tried to generate files differently

--- script.js
+++ script.js
@@ -1,69 +1,100 @@
 const pointsrx = / Points Grade <[^>]+MaxPoints:(\d+)/;
 const numeratorx = / Numerator$/;
 const studentidrx = /\d{6}/;
+const osiris_fake_headers = [
+    [],
+    ["PLEASE COPY THE CONTENTS BELOW"],
+    [],
+    [],
+    ["INTO A VALID OSIRIS SPREADSHEET"],
+    [],
+    [],
+    ["Student number","Name","Test date","Grade"],
+];
+
+let osiris_headers = [];
+
+let course_code = 'manual-merge';
 let brightspace_content = [];
 let selected_grades = [];
 let name_columns = [0];
 let grade_columns = [];
 let now = new Date();
-let ac_year = now.getFullYear();
-if (now.getMonth() < 8) {
-    ac_year--;
-}
 let now_date = now.toISOString().split(/[-T]/).slice(0,3).reverse().join('-');
 
-let osiris_content = [
-    ["Course","COURSE-CODE",undefined,undefined,"Time"],
-    ["Name","Course Name"],
-    ["Academic year",ac_year.toString()],
-    ["Test","TESTCODE","Assignment Name"],
-    ["Block","YEAR",undefined,"Grading scale","1-DEC NUM+GK"],
-    ["Opportunity","1"],
-    [],
-    ["Student number","Name","Test date","Grade"]
-];
+let osiris_students_ids = [];
+let using_real_osiris_spreadsheet = false;
+let osiris_workbook = XLSX.utils.book_new();
+let osiris_worksheet = XLSX.utils.aoa_to_sheet(osiris_fake_headers);
+XLSX.utils.book_append_sheet(osiris_workbook, osiris_worksheet, "Test list");
+
+const header_range_end = 7;
+
+function deleteAllGrades() {
+    let range = XLSX.utils.decode_range(osiris_worksheet["!ref"]);
+    console.log(JSON.stringify(range));
+    if (range.e.r > header_range_end) {
+        for (let r = header_range_end + 1; r <= range.e.r; r++) {
+            for (let c = range.s.c; c <= range.e.c; c++) {
+                let addr = XLSX.utils.encode_cell({r:r, c:c});
+                delete osiris_worksheet[addr];
+            }
+        }
+        range.e.r = header_range_end;
+        osiris_worksheet["!ref"] = XLSX.utils.encode_range(range);
+    }   
+}
 
 function getIntersection() {
-    let osiris_student_ids = osiris_content.slice(8).map(c => c[0]);
-    return selected_grades.filter(c => osiris_student_ids.includes(c[0]));
+    return selected_grades.filter(c => osiris_students_ids.includes(c[0]));
 }
 
 function displayIntersection() {
     let intersectiondescription = 'No grades to export';
     if (selected_grades.length) {
-        if (osiris_content.length < 9) {
-            intersectiondescription = 'The Osiris file is blank. All grades from Brightspace will be exported';
+        if (!using_real_osiris_spreadsheet) {
+            intersectiondescription = 'All grades from Brightspace will be exported in a way that you need to copy and paste manually into an Osiris spreadsheet';
         } else {
-            intersectiondescription = getIntersection().length + ' students are both in the Osiris and Brightspace lists. Only these students will be exported';
+            let intersection_length = getIntersection().length;
+            intersectiondescription = intersection_length + ' students are both in the Osiris and Brightspace lists.';
+            if (intersection_length < selected_grades.length) {
+                intersectiondescription += ' Only these students will be exported.';
+            } 
         }
     }
     document.getElementById('contentdescription').textContent = intersectiondescription;
 }
 
 function generateSpreadsheet() {
-    let grades_to_export = (osiris_content.length < 9) ? selected_grades : getIntersection();
+    let grades_to_export = using_real_osiris_spreadsheet ? getIntersection() : selected_grades;
     if (!grades_to_export.length) {
         alert('No grades to export');
     }
-    let final_table = osiris_content.slice(0,8).concat(
+    deleteAllGrades();
+    XLSX.utils.sheet_add_aoa(
+        osiris_worksheet,
         grades_to_export.map(g => [
             g[0],
             g[1],
             now_date,
             g[2].toString().replace('.',','),
-        ])
+        ]),
+        { origin: "A9" }
     );
-    let workbook = XLSX.utils.book_new();
-    let worksheet = XLSX.utils.aoa_to_sheet(final_table);
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Test list");
-    XLSX.writeFile(workbook, `Generated-Osiris-${osiris_content[0][1]}-${now_date}.xlsx`,{ compression: true });
+
+    XLSX.writeFile(osiris_workbook, `Generated-Osiris-${course_code}-${now_date}.xlsx`,{ compression: true });
 }
 
 function displayOsiris() {
-    document.getElementById('osirisheaders').innerHTML = osiris_content.slice(0,6).map(
-        l => '<tr>' + l.map(c => `<td>${c||''}</td>`).join('') + '</tr>'
-    ).join('');
-    document.getElementById('osiriscount').textContent = osiris_content.slice(8).length;
+    if (using_real_osiris_spreadsheet) {
+        document.getElementById('osirisheaders').innerHTML = osiris_headers.map(
+            l => '<tr>' + l.map(c => `<td>${c||''}</td>`).join('') + '</tr>'
+        ).join('');
+        document.getElementById('osiriscount').textContent = osiris_students_ids.length;
+        document.getElementById('osirisdata').style.display = 'block';
+    } else {
+        document.getElementById('osirisdata').style.display = 'none';
+    }
     displayIntersection();
 }
 
@@ -78,7 +109,7 @@ function selectGrades() {
         );
         let full_column = grade_columns[column];
         selected_grades = filled_in_grades.map(g => {
-            let denominator = full_column.denominator || g[column+1];
+            let denominator = full_column.denominator || g[column+1] || 100;
             return [
                 (studentidrx.exec(g[0])||["Invalid Student ID"])[0],
                 name_columns.map(n => g[n]).join(', '),
@@ -149,9 +180,15 @@ document.getElementById('osirisfile').addEventListener('change', e => {
     }
     var reader = new FileReader();
     reader.onload = f => {
-        let workbook = XLSX.read(reader.result);
-        let worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        osiris_content = XLSX.utils.sheet_to_json(worksheet, {header: 1});
+        osiris_workbook = XLSX.read(reader.result);
+        osiris_worksheet = osiris_workbook.Sheets[osiris_workbook.SheetNames[0]];
+        using_real_osiris_spreadsheet = true;
+        console.log(osiris_worksheet);
+        let content = XLSX.utils.sheet_to_json(osiris_worksheet, {header: 1});
+        osiris_students_ids = content.slice(8).map(c => c[0]);
+        osiris_headers = content.slice(0,6);
+        course_code = content[0][1];
+        console.log(osiris_students_ids);
         displayOsiris();
     };
     reader.readAsArrayBuffer(file);
@@ -160,4 +197,3 @@ document.getElementById('osirisfile').addEventListener('change', e => {
 document.getElementById('gradecolumn').addEventListener('change', selectGrades);
 document.getElementById('exportbutton').addEventListener('click', generateSpreadsheet);
 
-displayOsiris();
